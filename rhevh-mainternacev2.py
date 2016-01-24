c0349772a34c11d9c7a1223e74559da016708bc3
qmin77 <qmin77@gmail.com>
2016-01-25 00:14:52 +0900

    files has been changed to apply while loop but...

--- rhevh-mainternacev2.py
+++ rhevh-mainternacev2.py
@@ -20,26 +20,13 @@ def _getServerListInput():
        migraTServerList = f.read().splitlines()
     return mainTServerList, migraTServerList
 
-def migrateVm(vm, host):
-    """
-    Migrate vm.
-    Parameters:
-     * vm - vm to be migrated
-     * host - host where the vm should be migrated
-    """
-    vm.migrate(params.Action(host=host))
-    waitForState(vm, states.vm.up, timeout=240)
-    LOGGER.info("Migrated VM '%s' to host '%s'" % (vm.get_name(), host.get_name()))
-
-
 if __name__ == '__main__':
 
    mainTServerList, migraTServerList = _getServerListInput()
-   print mainTServerList, migraTServerList
    vms_ids = []
    args_map = {}
    hmb=getHost_getVM()
-   vm, host = hmb.do_balance(vms_ids,mainTServerList,migraTServerList, args_map)
-   print "host is ", host.name
-   print "vm is ", vm.name
-   migrateVm(vm,host)
+   hmb.do_balance(vms_ids,mainTServerList,migraTServerList, args_map)
+   #print "host is ", host.name
+   #print "vm is ", vm.name
+   #  migrateVm(vm,host)
